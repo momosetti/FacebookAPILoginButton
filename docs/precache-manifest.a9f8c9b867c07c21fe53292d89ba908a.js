a70d88bc7050693856ab807a67f35eaf37e99cfe
momo setti <zinasetti@gmail.com>
2019-04-19 16:24:56 +0100

    add github page as homepage of the new production

---
+++ docs/precache-manifest.a9f8c9b867c07c21fe53292d89ba908a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f3a95099ff88e821553",
+    "url": "/FacebookAPILoginButton/static/js/runtime~main.7d62555a.js"
+  },
+  {
+    "revision": "f5d3885b4599205a16b5",
+    "url": "/FacebookAPILoginButton/static/js/main.6b876cb9.chunk.js"
+  },
+  {
+    "revision": "9cf386aef81f110ac447",
+    "url": "/FacebookAPILoginButton/static/js/2.8412fda2.chunk.js"
+  },
+  {
+    "revision": "f5d3885b4599205a16b5",
+    "url": "/FacebookAPILoginButton/static/css/main.71de0ca0.chunk.css"
+  },
+  {
+    "revision": "440e610458c797addf4720d0aef4f56b",
+    "url": "/FacebookAPILoginButton/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98566b75653d0570b402b0aedeab09bc.js"
+  "/FacebookAPILoginButton/precache-manifest.a9f8c9b867c07c21fe53292d89ba908a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/FacebookAPILoginButton/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
